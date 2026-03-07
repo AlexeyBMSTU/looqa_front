ed004acbdd589b212a0473df1d1ed6c76a518ee0
Алексей Нарыжный <a.naryzhnyy@vkteam.ru>
2026-03-07 19:51:27 +0300

    SPRINT-1: добавил линт

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,22 @@ import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
   eslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
+    ignores: ['dist/**/*'],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
       },
     },
     plugins: {
